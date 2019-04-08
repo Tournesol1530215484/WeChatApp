82fd88a72ffe35cf2fda379fa6d1deeb0a760a8a
Tournesol1530215484 <1530215484@qq.com>
2019-04-08 17:01:20 +0800

    提交文件

---
+++ Qiu/pages/img/img.js
@@ -0,0 +1,95 @@
+// pages/text/text.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        content: [
+            {
+                face: "/images/face2.jpg",
+                name: "满脸胡渣社会人",
+                title: "儿子叫了几个同学来家里玩.",
+                img: "/images/face2.jpg",
+                joke: 123,
+                comment: 456,
+                share: 789
+            },
+            {
+                face: "/images/face3.gif",
+                name: "湖北彭于晏",
+                title: "为什么要追那个富家女?",
+                img: "/images/face3.gif",
+                joke: 123,
+                comment: 456,
+                share: 789
+            },
+            {
+                face: "/images/face4.jpg",
+                name: "阿茶",
+                title: "去便利店买了24块钱的东西 ",
+                img: "/images/face4.jpg",
+                joke: 123,
+                comment: 456,
+                share: 79
+            }
+
+        ],
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
